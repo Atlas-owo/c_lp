f664fed1f76bd6a7fd4b5061658492073d055e31
Atlas-owo <atlas_w@outlook.com>
2018-10-26 13:28:24 +0800

    数据可视化

--- P1598/P1598/main.c
+++ P1598/P1598/main.c
@@ -10,16 +10,46 @@
 
 
 int main(int argc, const char * argv[]) {
-    char a[405] = {0};
+    char a[4][105] = {0};
+    int s[26] = {0};
 
-    int i = 0, j = 0;
-    do{
-        a[i] = getchar();
-        i++;
-        if(a[i] == '\n') j++;
-    }while(j<=4);
+    int i,j,count;
+    for(i = 0; i < 4; i++){
+        gets(a[i]);
+    }
     
-    printf("%s\n",a);
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 105; j++){
+            if(a[i][j] >= 'A' && a[i][j] <= 'Z'){
+                s[a[i][j]-'A']++;
+            }
+        }
+    }
+    
+    for(count = 0,i = 0; i < 26; i++){
+        if(s[i] > count)    count = s[i];
+    }
+    
+    for(; count > 0; count--){
+        for(i = 0; i < 26; i++){
+            if(count == s[i]){
+                printf("*");
+                s[i]--;
+            }
+            else{
+                printf(" ");
+            }
+            
+            if(i == 25){
+                printf("\n");
+            }
+            else printf(" ");
+        }
+    }
+    for(i = 'A'; i < 'Z'; i++){
+        printf("%c ",i);
+    }
+    printf("Z");
     
     return 0;
 }
---
+++ P1914/P1914/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  P1914
+//
+//  Created by Atlas.owo on 18/10/26.
+//  Copyright © 2018年 Atlas_Lain. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int n,i;
+    scanf("%d",&n);
+    char c[55] = {0};
+    scanf("%s",c);
+    
+    for(i = 0; i < 55; i++){
+        if(c[i] <= 'z' && c[i] >= 'a'){
+            c[i] = c[i] + n;
+            if(c[i] > 'z')  c[i] -= 26;
+        }
+        
+        
+    }
+    
+    printf("%s\n",c);
+    return 0;
+}
